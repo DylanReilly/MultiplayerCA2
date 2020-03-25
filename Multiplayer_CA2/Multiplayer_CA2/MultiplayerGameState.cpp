39b6d97e904472f637475892c14b22d375acaf1c
DylanReilly <dylanreilly81@gmail.com>
2020-03-25 12:52:58 +0000

    Error fixing

--- Multiplayer_CA2/Multiplayer_CA2/MultiplayerGameState.cpp
+++ Multiplayer_CA2/Multiplayer_CA2/MultiplayerGameState.cpp
@@ -442,16 +442,6 @@ void MultiplayerGameState::handlePacket(sf::Int32 packetType, sf::Packet& packet
 			itr->second->handleNetworkRealtimeChange(static_cast<Player::Action>(action), actionEnabled);
 	} break;
 
-	// New enemy to be created
-	case Server::SpawnEnemy:
-	{
-		float height;
-		sf::Int32 type;
-		float relativeX;
-		packet >> type >> height >> relativeX;
-
-	} break;
-
 	// Mission successfully completed
 	case Server::MissionSuccess:
 	{
